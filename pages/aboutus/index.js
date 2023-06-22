4306c3d6b0e19d11fa740e3058b274be95415b27
swatidurgekar <swatidurgekar@gmail.com>
2023-06-22 09:58:36 +0530

    Final touch to About US page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function AboutUs() {
-  return <h1>About Us</h1>;
+  return (
+    <Fragment>
+      <Link href="/aboutus/1">Yash</Link>
+      <br />
+      <Link href="/aboutus/2">Vaibhav</Link>
+      <br />
+      <Link href="/aboutus/3">Suresh</Link>
+      <br />
+    </Fragment>
+  );
 }
 
 export default AboutUs;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 function News() {
-  return <h1>news</h1>;
+  return (
+    <Fragment>
+      <h1>news</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default News;
